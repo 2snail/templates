7b28f4f0b0bf13ca8b9bf67312b1e2afd8797853
zhangaz1 <zhangaz1@hotmail.com>
2020-09-24 16:46:38 +0800

    fix templates

--- packages/create-lib/tpls/AppGenerator/src/index.test.ts
+++ packages/create-lib/tpls/AppGenerator/src/index.test.ts
@@ -2,7 +2,7 @@ import sayHi from './';
 
 describe('index', () => {
   test('say', async cb => {
-    const result = await sayHi('wolrd');
+    const result = await sayHi('world');
     expect(result).toEqual('hello, world!');
 
     cb();
