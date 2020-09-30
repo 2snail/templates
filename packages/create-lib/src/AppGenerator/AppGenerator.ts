aae9416f860032227fd1eb3bc9214f09dfb8ebde
zhangaz1 <zhangaz1@hotmail.com>
2020-09-30 09:10:16 +0800

    fix: add shelljs for log

--- packages/create-lib/src/AppGenerator/AppGenerator.ts
+++ packages/create-lib/src/AppGenerator/AppGenerator.ts
@@ -1,12 +1,13 @@
 import { Generator, winPath } from '@umijs/utils';
 import { join, basename } from 'path';
+import shell from 'shelljs';
 
 export default class AppGenerator extends Generator {
   async writing() {
     const cwd = winPath(this.cwd);
     const packageName = this.args.name || basename(cwd);
 
-    return await this.copyDirectory({
+    const ss = {
       context: {
         version: require('../../package').version,
         siteMode: this.args.site,
@@ -14,6 +15,15 @@ export default class AppGenerator extends Generator {
       },
       path: join(__dirname, '../../tpls/Appgenerator'),
       target: this.cwd,
-    });
+    };
+
+    console.log('xxx:', ss);
+    shell.echo('hello');
+    shell.echo(shell.pwd());
+    shell.ls('.').forEach(file => console.log(file));
+
+    await this.copyDirectory(ss);
+
+    shell.ls('.').forEach(file => console.log(file));
   }
 }
