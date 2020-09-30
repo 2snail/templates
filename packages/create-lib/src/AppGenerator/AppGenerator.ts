257fffb6d78918dd367c7a3b9e91b01af6386481
zhangaz1 <zhangaz1@hotmail.com>
2020-09-30 09:26:51 +0800

    fix: include copyDirctory

--- packages/create-lib/src/AppGenerator/AppGenerator.ts
+++ packages/create-lib/src/AppGenerator/AppGenerator.ts
@@ -1,6 +1,15 @@
-import { Generator, winPath } from '@umijs/utils';
-import { join, basename } from 'path';
+import {
+  Generator,
+  winPath,
+  chalk,
+  mkdirp,
+  Mustache,
+  glob,
+} from '@umijs/utils';
 import shell from 'shelljs';
+
+import { copyFileSync, readFileSync, statSync, writeFileSync } from 'fs';
+import { dirname, relative, join, basename } from 'path';
 
 export default class AppGenerator extends Generator {
   async writing() {
@@ -25,5 +34,38 @@ export default class AppGenerator extends Generator {
     await this.copyDirectory(ss);
 
     shell.ls('.').forEach(file => console.log(file));
+  }
+
+  copyTpl(opts: { templatePath: string; target: string; context: object }) {
+    const tpl = readFileSync(opts.templatePath, 'utf-8');
+    const content = Mustache.render(tpl, opts.context);
+    mkdirp.sync(dirname(opts.target));
+    console.log(`${chalk.green('Write:')} ${relative(this.cwd, opts.target)}`);
+    writeFileSync(opts.target, content, 'utf-8');
+  }
+
+  copyDirectory(opts: { path: string; context: object; target: string }) {
+    const files = glob.sync('**/*', {
+      cwd: opts.path,
+      dot: true,
+      ignore: ['**/node_modules/**'],
+    });
+    console.log('files:', files);
+    files.forEach(file => {
+      const absFile = join(opts.path, file);
+      if (statSync(absFile).isDirectory()) return;
+      if (file.endsWith('.tpl')) {
+        this.copyTpl({
+          templatePath: absFile,
+          target: join(opts.target, file.replace(/\.tpl$/, '')),
+          context: opts.context,
+        });
+      } else {
+        console.log(`${chalk.green('Copy: ')} ${file}`);
+        const absTarget = join(opts.target, file);
+        mkdirp.sync(dirname(absTarget));
+        copyFileSync(absFile, absTarget);
+      }
+    });
   }
 }
